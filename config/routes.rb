1b29f88aed595805d8d24e5660d4b49f4fea01b1
kimiko ando <kimiko.ando13@icloud.com>
2023-11-07 16:59:15 +0900

    uncommented the index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index'
 
   # Create tasks
   get 'tasks/new', to: 'tasks#new', as: :new_task
